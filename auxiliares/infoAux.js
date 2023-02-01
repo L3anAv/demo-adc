6fd36d04baa495d0b69819087d182b8b68cd656d
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2023-02-01 11:39:17 -0300

    Updates

---
+++ auxiliares/infoAux.js
@@ -0,0 +1,8 @@
+
+/* Opciones de menu -> */
+export const opcionesMenu = [
+  { nombre: "INICIO", href: "/", id: 'inicio'},
+  { nombre: "QUIENES SOMOS", href: "/quienes-somos", id: 'quienes-somos'},
+  { nombre: "SERVICIOS", href: "/servicios", id: 'servicios'},
+  { nombre: "CONTACTO", href: "/", id: 'contacto'}
+]
---
+++ auxiliares/infoFichas.js
@@ -0,0 +1,89 @@
+const ArrayDatos = [
+    [
+      { 
+        "Svg":"Transporte",
+        "Row":"3/4",
+        "Colum":"2/3",
+        "RowResponsive":"3/4",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Transporte de cargas generales y peligrosas.", "- Fletes y mudanzas."]
+      },
+      {
+        "Svg": "Acarreo",
+        "Row":"3/4",
+        "Colum":"3/4",
+        "RowResponsive":"5/6",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Acarreo de autos/maquinaria."]
+      },
+      {
+        "Svg": "Desagote",
+        "Row":"4/5",
+        "Colum":"2/3",
+        "RowResponsive":"6/7",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Desagotes."]
+      },
+      {
+      "Svg": "Volquete",
+      "Row":"4/5",
+      "Colum":"3/4",
+      "RowResponsive":"7/8",
+      "ColumnResponsive":"2/4",
+      "LiContenido": ["- Volquetes."]
+      }
+    ],
+    [
+      {
+        "Svg": "Autoelevador",
+        "Row":"3/4",
+        "Colum":"2/3",
+        "RowResponsive":"3/4",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Autoelevadores."]
+      },
+      {
+        "Svg": "banoQuimico",
+        "Row":"3/4",
+        "Colum":"3/4",
+        "RowResponsive":"5/6",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Baños químicos."]
+      },
+      {
+        "Svg": "Bobcat",
+        "Row":"4/5",
+        "Colum":"2/3",
+        "RowResponsive":"6/7",
+        "ColumnResponsive":"2/4",
+        "LiContenido": ["- Bobcat."]
+      },
+      {
+      "Svg": "Grua",
+      "Row":"4/5",
+      "Colum":"3/4",
+      "RowResponsive":"7/8",
+      "ColumnResponsive":"2/4",
+      "LiContenido": ["- Gruas hidraúlicas."]
+      }
+    ],
+    [{
+      "Svg": "Deposito",
+      "Row":"3/4",
+      "Colum":"2/3",
+      "RowResponsive":"3/4",
+      "ColumnResponsive":"2/4",
+      "LiContenido": ["- Almacenamiento y gestión en nuestros depósitos."]
+    },
+    {
+      "Svg": "DepositoCarro",
+      "Row":"3/4",
+      "Colum":"3/4",
+      "RowResponsive":"5/6",
+      "ColumnResponsive":"2/4",
+      "LiContenido": ["- In/Out.", "- Repartos."]
+    }
+  ]
+  ]
+
+  export default ArrayDatos
